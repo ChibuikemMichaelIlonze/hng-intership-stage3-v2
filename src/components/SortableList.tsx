9da12efc7ea6deb8a589f6109ecfee26cdfdaf33
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-09-22 17:16:21 +0100

    i made usd of Sortablejs instead of react api

--- src/components/SortableList.tsx
+++ src/components/SortableList.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/config";
 import { useNavigate } from "react-router-dom";
+import Sortable from "sortablejs";
 
 const SortableList: React.FC = () => {
   const imageFilenames = [
@@ -25,7 +26,7 @@ const SortableList: React.FC = () => {
     "bicycle",
     "food",
     "car",
-    "river",
+    "mountain",
     "airplane",
     "skyscraper",
     "phone",
@@ -34,77 +35,62 @@ const SortableList: React.FC = () => {
     "school",
   ];
 
-  const [imageOrder, setImageOrder] = useState<number[]>(
-    Array.from({ length: imageFilenames.length }, (_, index) => index)
-  );
+  const [searchInput, setSearchInput] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const [dragItemIndex, setDragItemIndex] = useState<number | undefined>();
-  const [dragOverItemIndex, setDragOverItemIndex] = useState<
-    number | undefined
-  >();
+  const navigate = useNavigate();
 
-  const [searchInput, setSearchInput] = useState(""); // State for search input
-  const [loading, setLoading] = useState(true); // Loading state
-
-  const navigate = useNavigate(); // Initialize the navigate function
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    // Simulate an API call to load images
     const loadImages = async () => {
       try {
-        // Simulate loading delay
         await new Promise((resolve) => setTimeout(resolve, 2000));
-        setLoading(false); // Set loading to false when images are loaded
+        setLoading(false);
       } catch (error) {
         console.error("Error loading images:", error);
-        setLoading(false); // Set loading to false in case of an error
+        setLoading(false);
       }
     };
 
     loadImages();
   }, []);
 
-  const handleDragStart = (index: number) => {
-    setDragItemIndex(index);
-  };
+  useEffect(() => {
+    if (containerRef.current) {
+      new Sortable(containerRef.current, {
+        animation: 150,
+        onEnd: (event: any) => {
+          const { oldIndex, newIndex } = event;
 
-  const handleDragOver = (event: React.DragEvent) => {
-    event.preventDefault();
-  };
+          if (oldIndex !== newIndex) {
+            const reorderedTags = [...tags];
+            const reorderedFilenames = [...imageFilenames];
 
-  const handleDrop = (_index: number) => {
-    if (dragItemIndex !== undefined && dragOverItemIndex !== undefined) {
-      const newOrder = [...imageOrder];
-      const [draggedImage] = newOrder.splice(dragItemIndex, 1);
-      newOrder.splice(dragOverItemIndex, 0, draggedImage);
-      setImageOrder(newOrder);
+            const movedTag = reorderedTags.splice(oldIndex, 1)[0];
+            const movedFilename = reorderedFilenames.splice(oldIndex, 1)[0];
+
+            reorderedTags.splice(newIndex, 0, movedTag);
+            reorderedFilenames.splice(newIndex, 0, movedFilename);
+          }
+        },
+      });
     }
-  };
-
-  const handleDragEnter = (index: number) => {
-    setDragOverItemIndex(index);
-  };
-
-  const handleDragLeave = () => {
-    setDragOverItemIndex(undefined);
-  };
-
-  const handleDragEnd = () => {
-    setDragItemIndex(undefined);
-    setDragOverItemIndex(undefined);
-  };
+  }, [tags, imageFilenames]);
 
   const handleLogout = async () => {
     try {
       await signOut(auth);
-
       navigate("/signup");
     } catch (error) {
       console.error("Logout error:", error);
     }
   };
 
-  const filteredImageOrder = imageOrder.filter((index) =>
+  const filteredImageOrder = Array.from(
+    { length: imageFilenames.length },
+    (_, index) => index
+  ).filter((index) =>
     tags[index].toLowerCase().includes(searchInput.toLowerCase())
   );
 
@@ -141,30 +127,27 @@ const SortableList: React.FC = () => {
           <span className="loading loading-spinner loading-lg"></span>
         </div>
       ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 justify-between gap-5 mt-10 px-6">
-          {filteredImageOrder.map((index) => (
-            <div
-              key={index}
-              className="relative flex justify-center items-center"
-            >
-              <div className="absolute top-2 left-5 lg:left-10 bg-gray-800 text-white px-2 py-1 rounded ">
-                {tags[index]}
+        <div>
+          <div
+            ref={containerRef}
+            className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 justify-between gap-5 mt-10 px-6"
+          >
+            {filteredImageOrder.map((index) => (
+              <div
+                key={index}
+                className="relative flex justify-center items-center "
+              >
+                <div className="absolute top-2 left-5 lg:left-10 bg-gray-800 text-white px-2 py-1 rounded ">
+                  {tags[index]}
+                </div>
+                <img
+                  src={`/images/${imageFilenames[index]}`}
+                  alt={`Image ${index + 1}`}
+                  className="h-80 w-56 object-cover "
+                />
               </div>
-              <img
-                src={`/images/${imageFilenames[index]}`}
-                alt={`Image ${index + 1}`}
-                className="h-80 w-56 object-cover "
-                draggable
-                onDragStart={() => handleDragStart(index)}
-                onDragOver={handleDragOver}
-                onDrop={() => handleDrop(index)}
-                onDragEnter={() => handleDragEnter(index)}
-                onDragLeave={handleDragLeave}
-                onDragEnd={handleDragEnd}
-                onLoad={() => setLoading(false)}
-              />
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
     </div>
